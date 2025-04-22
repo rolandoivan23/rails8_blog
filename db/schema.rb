169170be69fa51c969506a57e0bea5555370316a
Rolando Vazquez <rolando.vazquez.23@gmail.com>
2025-04-22 01:51:52 +0000

    Categories scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_20_011627) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_22_014621) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -47,6 +47,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_20_011627) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
