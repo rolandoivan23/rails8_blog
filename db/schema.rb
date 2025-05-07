11e4635b3cc3c94ec089202da9b732e3b39bd5d7
Rolando Vazquez <rolando.vazquez.23@gmail.com>
2025-05-07 16:52:27 -0600

    Colum likes added to comments and html rendred reading from db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_06_214938) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_07_220735) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -85,6 +85,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_06_214938) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes", default: 0, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
