74e9517afe9baa5bdb36d5bdd1ae1aadd85b2533
Rolando Vazquez <rolando.vazquez.23@gmail.com>
2025-05-15 20:39:01 -0600

    Basic implementation of the followers/following users modal with funcions and onclik events(temporaly due to an issue with js in the rails way )

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,16 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :set_current_user
+  before_action :set_current_user, :set_followers, :set_following_users
   helper_method :current_user, :logged_in?
+
+  def set_followers
+    @followers ||= current_user.followers_users
+  end
+
+  def set_following_users
+    @following_users ||= current_user.following_users_users
+  end
 
   private
     def set_current_user
