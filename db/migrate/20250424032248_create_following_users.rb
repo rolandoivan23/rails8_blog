17a2943940b78def54151cf849dbd38dab330cd6
Rolando Vázquez <rolando.vazquez.23@gmail.com>
2025-05-26 23:43:35 -0600

    Fixes for initial setup. A migration that had a duplicated field was fixed and also seeds for user's password confirmation

--- db/migrate/20250424032248_create_following_users.rb
+++ db/migrate/20250424032248_create_following_users.rb
@@ -1,7 +1,7 @@
 class CreateFollowingUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :following_users do |t|
-      t.references :user, null: false, foreign_key: true
+      #t.references :user, null: false, foreign_key: true
       t.references :user, :following_user_id, null: false, foreign_key: true
 
       t.timestamps
